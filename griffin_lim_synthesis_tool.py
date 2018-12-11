2313cdcda3ce7a1dab2dfd53343e5248568d034f
a8568730 <a8568730@gmail.com>
2018-12-11 07:13:02 +0000

    * griffin_lim_synthesis_tool.py, pass input mel_file and output path from terminal.

--- griffin_lim_synthesis_tool.py
+++ griffin_lim_synthesis_tool.py
@@ -1,18 +1,25 @@
 import numpy as np
 from datasets.audio import *
-import os
+from sys import argv
 from hparams import hparams
 
-out_dir = 'wav_out'
+# After Tacotron-2 training, 
+# if you only want to run Tacotron synthesis, check the script below:
+#
+# 1. Run Tacotron model evaluation
+# $ python synthesize.py --model='Tacotron' --mode='eval' --name='Tacotron-2' --text_list ./custom_test_sentence.txt
+#
+# 2. Output audio
+# $ python griffin_lim_synthesis_tool.py tacotron_output/eval/mel-batch_0_sentence_0.npy wav_out/test_0.wav
+#
 
-os.makedirs(out_dir, exist_ok=True)
+mel_file = argv[1]
+output_file = argv[2]
 
-mel_file = 'mel-batch_1_sentence_0.npy'#'training_data/mels/mel-LJ001-0005.npy'
-mel_spectro = np.load('tacotron_output/eval/' + mel_file)
+mel_spectro = np.load(mel_file)
 wav = inv_mel_spectrogram(mel_spectro.T, hparams) 
 #save the wav under test_<folder>_<file>
-save_wav(wav, os.path.join(out_dir, 'test_mel_{}.wav'.format(mel_file.replace('/', '_').replace('\\', '_').replace('.npy', ''))),
-        sr=hparams.sample_rate)
+save_wav(wav, output_file, sr=hparams.sample_rate)
 
 
 
